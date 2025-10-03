ce798761b63da9da8660bf8f5ae5ed30779fe593
shivsharma-cmyk <shiv.sharma181108@gmail.com>
2025-10-03 09:27:59 -0600

    comments added

--- interactive-scene/sketch.js
+++ interactive-scene/sketch.js
@@ -7,7 +7,7 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
-
+//All the variables used in the code.
 let xPos = 200;
 let yPos = 0;
 let speed = 5;
@@ -18,10 +18,12 @@ let w = 200;
 let h = 100;
 let gameState = "start";
 
+//Making the canvas.
 function setup() {
   createCanvas(400, 400);
 }
 
+//Using all the functions in it and divided it with start and game screen
 function draw(){
   if (gameState === "start") {
     background("lightblue");
@@ -40,12 +42,14 @@ function draw(){
     rectBox();
   }
 }  
-    
+
+//Making the balls for the game
 function showCircle(){
   fill("lightblue");
   circle(xPos, yPos += speed, 50);  
 }
 
+//Droping the ball down to catch
 function ballDrop(){
   if (yPos > height){
     yPos = 0;
@@ -54,6 +58,7 @@ function ballDrop(){
   }
 }  
 
+//If the ball caught score a point and increase speed
 function ballPoint(){
   if (dist(xPos, yPos, mouseX, 350)< 25){
     yPos = 0;
@@ -63,22 +68,26 @@ function ballPoint(){
   }
 }
 
+//Making the score board
 function scoreText(){
   textSize(25);
   text("Score:", 25, 50);
   text(score, 100, 50);
 }
 
+//Making the slider to catch the balls
 function rectBox(){
   rectMode(CENTER);
   rect(mouseX,350, 100, 25);
 }  
 
+//Start button on start screen
 function showButton() {
   fill("white");
   rect(rectX, rectY, w, h);
 }
 
+//Making the start button work if pressed
 function mousePressed() {
   if (gameState === "start") {
     if (mouseX >= rectX && 
@@ -90,6 +99,7 @@ function mousePressed() {
   }
 }
 
+//Title and start text for the start screen
 function showText() {
   textSize(75);
   textStyle(BOLDITALIC);
@@ -97,6 +107,8 @@ function showText() {
   text("Start", width/3.5, 225);
   text("Ball Catch", width/20, height/4);
 }
+
+//Text for the instruction on the start screen
 function showInstruction(){
   textSize(25);
   textStyle(BOLDITALIC);
