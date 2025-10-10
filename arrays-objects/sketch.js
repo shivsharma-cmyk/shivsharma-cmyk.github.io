fa8f08ad3854fcb16926db92566268b70a968493
shivsharma-cmyk <shiv.sharma181108@gmail.com>
2025-10-10 08:55:01 -0600

    new project array and object

---
+++ arrays-objects/sketch.js
@@ -0,0 +1,17 @@
+// Project Title
+// Block Stacker
+// Your Name
+//Shivansh Sharma
+// Date
+//10/10/2025
+// Extra for Experts:
+// - describe what you did to take this project "above and beyond"
+
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  background(220);
+}
