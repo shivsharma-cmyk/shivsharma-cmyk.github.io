4427c3cd2ca1a7afb408d6fe7ee14c71275130c9
shivsharma-cmyk <shiv.sharma181108@gmail.com>
2025-10-15 09:53:04 -0600

    array update

--- arrays-objects/sketch.js
+++ arrays-objects/sketch.js
@@ -7,11 +7,21 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
+const blockWidth = 300;
+const blockheight = 30;
+let r;
+let g;
+let b;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  r = random(255);
+  g = random(255);
+  b = random(255);
 }
 
 function draw() {
   background(220);
+
+  rect(windowWidth/2, windowHeight/2, blockWidth, blockheight);
 }
