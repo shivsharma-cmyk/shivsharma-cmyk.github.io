ab987dc0648958b02b76108b03b6d85e517f588e
shivsharma-cmyk <shiv.sharma181108@gmail.com>
2025-10-21 09:40:32 -0600

    added starting screen

--- arrays-objects/sketch.js
+++ arrays-objects/sketch.js
@@ -9,6 +9,11 @@
 
 const blockWidth = 300;
 const blockheight = 30;
+let rectX = 100;
+let rectY = 150;
+let w = width/2;
+let h = height/2;
+let gameState = "start";
 let r;
 let g;
 let b;
@@ -22,8 +27,17 @@ function setup() {
 }
 
 function draw() {
-  background(220);
-  spawnRectangle();
+  if (gameState === "start") {
+    background("lightblue");
+    showButton();
+    showText();
+    showInstruction();
+  }
+
+  if (gameState === "ball") {
+    background(220);
+    spawnRectangle();
+  }
 }
 
 function spawnRectangle() {
@@ -33,4 +47,38 @@ function spawnRectangle() {
 
 function dropRectangle() {
   
+}
+
+function mousePressed() {
+  if (gameState === "start") {
+    if (mouseX >= rectX && 
+        mouseX <= rectX + w &&
+        mouseY >= rectY && 
+        mouseY <= rectY + h) {
+      gameState = "ball";
+    }
+  }
+}
+
+function showButton() {
+  fill("white");
+  rect(rectX, rectY, w, h);
+}
+
+//Title and start text for the start screen
+function showText() {
+  textSize(75);
+  textStyle(BOLDITALIC);
+  fill("black");
+  text("Start", width/3.5, 225);
+  text("Block Stack", width/2, height/2);
+}
+
+//Text for the instruction on the start screen
+function showInstruction(){
+  textSize(25);
+  textStyle(BOLDITALIC);
+  fill("black");
+  text("Use the arrow keys to \n stack the blocks to the top",
+    width/10, 350);
 }
