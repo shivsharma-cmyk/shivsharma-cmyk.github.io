f905a5545f220938e449452cb438cf34c9183d36
shivsharma-cmyk <shiv.sharma181108@gmail.com>
2025-11-03 09:53:11 -0600

    made the grid base

--- 2d-grid-game/sketch.js
+++ 2d-grid-game/sketch.js
@@ -6,10 +6,27 @@
 // - describe what you did to take this project "above and beyond"
 
 
+
+
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
 }
 
 function draw() {
   background(220);
+}
+
+function showGrid() {
+  for (let y = 0; y < SQUARE_DIMENSIONS; y++) {
+    for (let x = 0; x < SQUARE_DIMENSIONS; x++) {
+      if (theGrid[y][x] === 1) {
+        fill("black");
+      }
+      else if (theGrid[y][x] === 0) {
+        fill("white");
+      }
+      square(x * cellSize, y * cellSize, cellSize);
+    }
+  }
 }
